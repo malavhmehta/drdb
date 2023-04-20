fa6350c221dce1c8efff6e3ee319693dc0ab6456
malav-mehta <malavhmehta@outlook.com>
2023-04-19 20:28:49 -0400

    Use uint8_t

--- src/kv/encoder.h
+++ src/kv/encoder.h
@@ -1,18 +1,19 @@
 #ifndef DRDB_ENCODER_H
 #define DRDB_ENCODER_H
 
+#include <cstdint>
 #include <vector>
 
 namespace kv
 {
 class Encoder
 {
-    public:
+public:
 	static constexpr size_t METADATA_SIZE = 2;
 	static constexpr size_t MAX_RECORD_SIZE = (1 << (METADATA_SIZE * 8)) - 2;
 	static const std::vector<uint8_t> TOMBSTONE_VALUE;
 
-    public:
+public:
 	static std::vector<uint8_t>
 	encode_bytes(const std::vector<uint8_t>& input, size_t from = 0, size_t to = -1);
 
